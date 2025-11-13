4b6fa6c5815ebd652aac41f0248adc3ed00fece8
Akhil A <info.kid4lyfe@gmail.com>
2025-11-13 02:43:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7174307005-ed7f9",
+  "appId": "1:984075256723:web:ae970de931ce3af95cde66",
+  "apiKey": "AIzaSyCvncHuv0i_ttCsPHyEs-8Z8QeHfiNyXus",
+  "authDomain": "studio-7174307005-ed7f9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "984075256723"
+};
